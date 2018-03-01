025104be59f4e3cba4e25fff0ff8a446e6f20ee2
lijianxi <lijianxi@tsingyunsolar.com>
2018-03-01 14:47:59 +0800

    hello

---
+++ src/main/java/com/riki/demo/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.riki.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class HelloController{
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!!!";
+    }
+}
